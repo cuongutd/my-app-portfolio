9a2194a54ad056c625ba4e61ed9a7b870efd1d84
root <root@ip-10-47-174-141.ec2.internal>
2015-06-01 07:30:21 +0000

    Committed by Udacity

---
+++ app/src/main/java/com/appportfolio/cuong/appportfolio/MainActivity.java
@@ -0,0 +1,69 @@
+package com.appportfolio.cuong.appportfolio;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void buttonClick(String message){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, message, duration);
+        toast.show();
+
+    }
+
+    public void buttonSpotifyClick(View view){
+        buttonClick("This will launch my app Spotify!");
+    }
+    public void buttonFootballClick(View view){
+        buttonClick("This will launch my app Football Scores!");
+    }
+    public void buttonLibraryClick(View view){
+        buttonClick("This will launch my app Library!");
+    }
+    public void buttonBuildClick(View view){
+        buttonClick("This will launch my app Build It Bigger!");
+    }
+    public void buttonXYZClick(View view){
+        buttonClick("This will launch my app XYZ Reader!");
+    }
+    public void buttonCapstoneClick(View view){
+        buttonClick("This will launch my capstone app!");
+    }
+}
